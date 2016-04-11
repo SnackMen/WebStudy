4461d8005f8c141bb2e5d0eeb8253c1ab7a3a609
SnackMan <vipkia@sina.cn>
2016-04-11 15:57:07 +0800

    完成教师课表查询和名单查询‘

--- StudentRegistrationSystem/Content/js/teacherIndex.js
+++ StudentRegistrationSystem/Content/js/teacherIndex.js
@@ -5,30 +5,32 @@
         $("#queryTimetable").css({ "display": "block" });
     });
     $("#input").click(function () {
+        $("#schedule-studentclass").empty();
         $("#queryTimetable").css({ "display": "none" });
         $("#inputgrade").css({ "display": "block" });
     });
     $("#shownavigation").on("click", "#0", this, function (event) {
-        //alert("aaa");
+        var name = $("#teacher-name-value").text();
         $("#showCourse").empty();
-        $.get("/Teacher/StudentList1", function (result) {
+        $.get("/Teacher/StudentList1", { tname: name }, function (result) {
             $("#showCourse").html(result);
         });
         $("#showCourse").css({ "display": "block" });
     });
     $("#shownavigation").on("click", "#1", this, function (event) {
-        //alert("aaa");
+        var name = $("#teacher-name-value").text();
         $("#showCourse").empty();
-        $.get("/Teacher/StudentList2", function (result) {
+        $.get("/Teacher/StudentList2", { tname: name }, function (result) {
             $("#showCourse").html(result);
         });
         $("#showCourse").css({ "display": "block" });
     });
     $("#shownavigation").on("click", "#schedule", this, function (event) {
+        var name = $("#teacher-name-value").text();
         $("#showCourse").empty();
-        $.get("/Teacher/QueryTable", { tname: ViewBag.tname }, function (result) {
+        $.get("/Teacher/QueryTable", { tname: name }, function (result) {
             $("#showCourse").html(result);
-        })
+    });
         $("#showCourse").css({ "display": "block" });
     });
 })
--- StudentRegistrationSystem/Controllers/TeacherController.cs
+++ StudentRegistrationSystem/Controllers/TeacherController.cs
@@ -27,7 +27,7 @@ namespace StudentRegistrationSystem.Controllers
 			int i = 0;
 			GradeDBContext grade = new GradeDBContext();
 			SelectCourseDBContext selectCname = new SelectCourseDBContext();
-			List<SelectCourse> cname = selectCname.SelectCourses.Where(u => u.TNAME == "王晓名").ToList();
+			List<SelectCourse> cname = selectCname.SelectCourses.Where(u => u.TNAME == tname).ToList();
 			foreach (var u in cname)
 			{
 				@TempData[i.ToString()] = u.CNAME;
@@ -43,12 +43,75 @@ namespace StudentRegistrationSystem.Controllers
 			List<SelectCourse> result = selectCourse.SelectCourses.Where(u => u.TNAME == tname).ToList();
 			return View(result);
 		}
-		public ActionResult StudentList1()
+		public ActionResult StudentList1(string tname)
+		{
+			int i = 0;
+			SelectedCourseDBContext selectSno = new SelectedCourseDBContext();
+			BasicInfoDBContext studentMessage = new BasicInfoDBContext();
+			SelectCourseDBContext selectCname = new SelectCourseDBContext();
+			List<SelectedCourse> studentSno = selectSno.SelectedCourses.Where(u => u.TNAME == tname).ToList();
+			List<SelectCourse> cname = selectCname.SelectCourses.Where(u => u.TNAME == tname).ToList();
+			string[] className = new string[10];
+			List<BasicInfo> result = new List<BasicInfo>();
+			foreach (var n in cname)
+			{
+				className[i] = n.CNAME;
+				i++;
+			}
+			foreach (var u in studentSno)
+			{
+				string name = className[0];
+				if (name == u.CNAME)
+				{
+					List<BasicInfo> message = studentMessage.Basics.Where(m => m.ID == u.SNO && u.CNAME == name && u.SEMESTER == "15-16春").ToList();
+					result.AddRange(message);
+				}
+			}
+			return View(result);
+		}
+		public ActionResult StudentList2(string tname)
+		{
+			int i = 0;
+			SelectedCourseDBContext selectSno = new SelectedCourseDBContext();
+			SelectCourseDBContext selectCname = new SelectCourseDBContext();
+			BasicInfoDBContext studentMessage = new BasicInfoDBContext();
+			List<SelectedCourse> studentSno = selectSno.SelectedCourses.Where(u => u.TNAME == tname).ToList();
+			List<SelectCourse> cname = selectCname.SelectCourses.Where(u => u.TNAME == tname).ToList();
+			string[] className = new string[cname.Count];
+			List<BasicInfo> result = new List<BasicInfo>();
+			foreach (var n in cname)
+			{
+				className[i] = n.CNAME;
+				i++;
+			}
+			foreach (var u in studentSno)
+			{
+				string name = className[1];
+				if (name == u.CNAME)
+				{
+					List<BasicInfo> message = studentMessage.Basics.Where(m => m.ID == u.SNO && u.CNAME == name && u.SEMESTER == "15-16春").ToList();
+					result.AddRange(message);
+				}
+				
+			}
+			return View(result);
+		}
+		public ActionResult StudentGrade(string tame)
 		{
 			return View();
 		}
-		public ActionResult StudentList2()
+		public ActionResult CourseSchedule(string tname)
 		{
+			int i = 0;
+			GradeDBContext grade = new GradeDBContext();
+			SelectCourseDBContext selectCname = new SelectCourseDBContext();
+			List<SelectCourse> cname = selectCname.SelectCourses.Where(u => u.TNAME == "王晓名").ToList();
+			foreach (var u in cname)
+			{
+				@TempData[i.ToString()] = u.CNAME;
+				i++;
+			}
+			@ViewBag.count = i;
 			return View();
 		}
 	}
